97f8f6c78ed26446e975a8da0698e738a9690ce0
Dan Kang <dankang27@gmail.com>
2018-06-06 23:41:57 -0400

    render images and start lazy loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: {},
+      modal: false,
+      modalIdx: 99999
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          content: data
+        });
+      });
+  }
+
+  toggleModal(idx) {
+    this.setState({
+      modal: !this.state.modal,
+      modalIdx: idx
+    });
+    console.log(this.state.modalIdx)
+    console.log(idx.target)
+  }
+
+  allContent() {
+    return this.state.content.map((el, idx) => {
+      return (
+        <section key={idx} onClick={() => this.toggleModal(idx)}>
+          <img src={el.thumbnailUrl} alt={el.title}  />
+          {this.state.modal && this.state.modalIdx === idx ? (
+            <div className="picture-modal">
+              <img src={el.url} alt={el.title} />
+            </div>
+          ) : (
+            ""
+          )}
+        </section>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {Object.values(this.state.content).length > 0 ? this.allContent() : ""}
       </div>
     );
   }
